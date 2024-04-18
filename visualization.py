ad37626998d6dc81cec6bbe47d6846ebbc833c62
andricmitrovic <andricmitrovicnikola@yahoo.com>
2024-04-18 19:12:36 +0200

    Implemented evolution of stress as a function of total run time

--- visualization.py
+++ visualization.py
@@ -6,13 +6,22 @@ import glob
 import ast
 import re
 from matplotlib import pyplot as plt
-
+from sklearn.linear_model import LinearRegression
 
 def clean(x):
     x = x.strip('[]').strip()
     x = re.sub(r'\s+', ' ', x)
     x = x.split()
     return [int(num) for num in x]
+
+
+def sum_times(arr):
+    prefix_sum = []
+    current_sum = 0
+    for num in arr:
+        current_sum += num
+        prefix_sum.append(current_sum)
+    return prefix_sum
 
 
 def prep_data(path):
@@ -26,6 +35,9 @@ def prep_data(path):
 
     df['mwtoSave'] = df['mwtoSave'].apply(lambda x: re.sub(r'\s+', ',', x))
     df['mwtoSave'] = df['mwtoSave'].apply(lambda x: np.squeeze(ast.literal_eval(x)))
+
+    # Apply the prefix sum function to each row in the DataFrame
+    df['ExecTimeEnd'] = df['timetoSave'].apply(sum_times)
 
     return df
 
@@ -155,6 +167,58 @@ def visualize_2d_time_stress(operator):
         plt.close()
 
 
+def visualize_strees_over_time(operator):
+    # Get data
+    path = './data/csv/RESCH/P0' + str(operator) + '.csv'
+    df = prep_data(path)
+    df = df[['ExecTimeEnd', 'mwtoSave']]
+
+
+    combined_data = []
+    for index, row in df.iterrows():
+        combined_data.extend(zip(row['ExecTimeEnd'], row['mwtoSave']))
+
+    filtered_data = [(et, mw) for et, mw in combined_data if mw != 0]
+    filtered_exec_times, filtered_mwtosave = zip(*filtered_data)
+
+    # Plotting scatter
+    plt.figure(figsize=(10, 6))
+    plt.scatter(filtered_exec_times, filtered_mwtosave, marker='o', color='blue', alpha=0.8)
+    plt.title('Stress over time')
+    plt.xlabel('End time of the task')
+    plt.ylabel('Stress')
+
+
+    #############
+
+    # Reshape the data for LinearRegression
+    X = np.array(filtered_exec_times).reshape(-1, 1)  # Reshape to a column vector
+    y = np.array(filtered_mwtosave)
+
+    # Fit linear regression model
+    model = LinearRegression()
+    model.fit(X, y)
+
+    # Predict y values using the model
+    y_pred = model.predict(X)
+
+    # Plot the scatter plot
+    plt.scatter(filtered_exec_times, filtered_mwtosave, label='Data Points')
+
+    # Plot the regression line
+    plt.plot(filtered_exec_times, y_pred, color='red', label='Linear Regression')
+    ############
+
+    plt.legend()
+    plt.grid(True)
+
+    # Create output destination
+    dir_path = f"./output/visualize_stress_totaltime/"
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+    plt.savefig(f'{dir_path}/operator_{operator}.png')
+
+
 if __name__ == '__main__':
     # standar_exec_times = {'1': 0.5,
     #             '2': 0.667,
@@ -172,4 +236,5 @@ if __name__ == '__main__':
     for i in [1, 2, 3, 4, 6, 7]:            # todo: some error generating csv for operator 5
         histogram_exec_times_op(operator = i)
         visualize_2d_time_stress(operator = i)
+        visualize_strees_over_time(operator = i)
 
