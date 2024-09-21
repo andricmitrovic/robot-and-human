f636943fdba69a0ff3357af44ae4c2960e23239f
andricmitrovic <andricmitrovicnikola@yahoo.com>
2024-09-21 13:43:13 +0200

    Changed probability modeling to log-normal and gamma and scaled histogram to match the bell curve

--- modeling.py
+++ modeling.py
@@ -3,7 +3,7 @@ import pandas as pd
 import os
 from matplotlib import pyplot as plt
 from utils import prep_data
-from scipy.stats import gaussian_kde
+from scipy.stats import gaussian_kde, lognorm, gamma
 from scipy.stats import norm
 
 
@@ -57,43 +57,56 @@ def task_prob_dist(rescheduling = True):
             for i in range(len(tasks)):
                 task_data[str(tasks[i])].append(times[i])
 
-    with open(f"{dir_path}/gaussian_params.txt", "w") as file:
-        bin_count = 30
-        for key, times in task_data.items():
-            if not times:
-                continue
-            times = np.array(times)
+    file_ln = open(f"{dir_path}/params_ln.txt", "w")
+    file_gamma = open(f"{dir_path}/params_gamma.txt", "w")
 
-            plt.hist(times, bins = bin_count, color = 'skyblue', edgecolor = 'black')
-            plt.xlabel('Exec time')
-            plt.ylabel('Frequency')
-            plt.title(f'Histogram of Task {key}')
+    bin_count = 30
+    for key, times in task_data.items():
+        if not times:
+            continue
+        times = np.array(times)
 
-            x_values = np.linspace(min(times), max(times), 1000)
+        plt.hist(times, bins = bin_count, color = 'skyblue', edgecolor = 'black', density=True)
+        plt.xlabel('Exec time')
+        plt.ylabel('Frequency')
+        plt.title(f'Histogram of Task {key}')
 
-            ### Kernel density estimation (non parametric)
-            kde = gaussian_kde(times)
-            # Evaluate the KDE at a range of points
-            kde_values = kde.evaluate(x_values)
-            # Plot the estimated probability density function
-            plt.plot(x_values, kde_values, label='Gaussian KDE')
+        x_values = np.linspace(min(times), max(times), 1000)
 
-            ### Gaussian MLE (parametric)
-            mu, std = norm.fit(times)
+        ### Log-Normal MLE (parametric)
+        shape_ln, loc_ln, scale_ln = lognorm.fit(times, floc=0)  # fit log-normal with fixed location (0)
+        mu_ln = np.log(scale_ln)
+        sigma_ln = shape_ln
 
-            # Save params
-            line = f'{key} {mu} {std}\n'
-            file.write(line)
+        # Save log-normal params (mu_ln, sigma_ln)
+        #line_ln = f'{key} LogNormal mu={mu_ln:.4f} sigma={sigma_ln:.4f}\n'
+        line_ln = f'{key} {mu_ln} {sigma_ln}\n'
+        file_ln.write(line_ln)
 
-            plt.plot(x_values, norm.pdf(x_values, mu, std), label='Gaussian MLE')
-            plt.legend()
-            # plt.show()
-            plt.savefig(f'{dir_path}/task_{key}.png')
-            plt.close()
+        # Plot the log-normal PDF
+        plt.plot(x_values, lognorm.pdf(x_values, shape_ln, loc_ln, scale_ln), label='Log-Normal MLE')
+
+        ### Gamma MLE (parametric)
+        shape_g, loc_g, scale_g = gamma.fit(times, floc=0)  # fit gamma with fixed location (0)
+
+        # Save gamma params (shape_g, scale_g)
+        # line_g = f'{key} Gamma shape={shape_g:.4f} scale={scale_g:.4f}\n'
+        line_g = f'{key} {shape_g} {scale_g}\n'
+        file_gamma.write(line_g)
+
+        # Plot the gamma PDF
+        plt.plot(x_values, gamma.pdf(x_values, shape_g, loc_g, scale_g), label='Gamma MLE')
+
+        plt.legend()
+        plt.savefig(f'{dir_path}/task_{key}.png')
+        plt.close()
+
+    file_ln.close()
+    file_gamma.close()
 
     return task_data
 
 
 if __name__ == '__main__':
     # task_prob_dist()
-    task_prob_dist(rescheduling = False)
+    task_prob_dist(rescheduling = True)
